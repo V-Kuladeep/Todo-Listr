ba06b70340943822e7c5476874052bc218439df4
kuladeep <kuladeep14june@gmail.com>
2021-11-23 09:57:32 +0530

    this is Todo list

---
+++ button/button.js
@@ -0,0 +1,7 @@
+function addOn(){
+var textInput = document.createElement('div')
+
+textInput.innerHTML="<input id='typetext' type='text'/>"
+
+document.getElementById("lassi").appendChild(textInput);
+}
